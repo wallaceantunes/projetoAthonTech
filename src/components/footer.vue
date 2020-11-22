2a28c19cc3d4e3d8316bd1d0e146974a40a41e36
wallaceantunes <wallace.dantunes@gmail.com>
2020-11-22 03:18:20 -0200

    added new-crime-btn on footer to mobile and tablet

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="footer">
+        <div class="row hidden-pc">
+            <div class="col-tb-100p col-mb-100p">
+                <button class="btn btn-add block">
+                    <i class="fa fa-plus-square-o" aria-hidden="true" />
+                    ADD NEW CRIME
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -12,5 +20,11 @@
     height: 60px;
     opacity: 1;
     box-shadow: 0px -3px 3px #00000029;
+}
+.row{
+    padding-top: 15px;
+    padding-left: 20px;
+    padding-right: 20px;
+    justify-content: center;
 }
 </style>
