cddd4af9af2f2f3af6cb156a32cc546671f61b08
wallaceantunes <wallace.dantunes@gmail.com>
2020-11-23 04:23:10 -0200

    added layout new weapon

---
+++ src/views/newWeapon.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-pc-100p col-tb-100p col-mb-100p">
+                <b class="title">NEW WEAPON</b>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-pc-50p col-tb-50p col-mb-100p">
+                <Input label="Type of weapon" type="select"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-pc-50p col-tb-50p col-mb-100p">
+                <Input label="Weapon" type="text"/>
+            </div>
+        </div>
+        <div class="row hidden-mb">
+            <div class="col-pc-50p col-tb-50p">
+                <button class="btn btn-add block">
+                    <i class="fa fa-plus-square-o" aria-hidden="true" />
+                     SAVE NEW WEAPON
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import Input from '@/components/utils/inputComponent.vue'
+
+export default Vue.extend({
+  components: { Input }
+})
+</script>
+
+<style scoped>
+.title{
+  color: #313131;
+  font-family: Cinzel Decorative;
+  font-size: 30px;
+}
+@media screen and (max-width: 699px){
+    .content{
+        background-color: #FAFAFA;
+    }
+}
+</style>
