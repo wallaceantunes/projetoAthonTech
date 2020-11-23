84252eb1ff627783cce7013f4d4a9910ba43557c
wallaceantunes <wallace.dantunes@gmail.com>
2020-11-23 04:27:27 -0200

    added layout newCrime

---
+++ src/components/NewCrime/criminalComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-pc-50p col-tb-75p col-mb-100p">
+                <Input label="Criminal" placeholder="Type the name of the criminal..." type="text"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-pc-25p col-tb-50p col-mb-100p">
+                <Input label="Weapon (optional)" type="select"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-pc-25p col-tb-25p col-mb-50p row-margin">
+                <button class="btn btn-white block">
+                    <i class="fa fa-plus-square-o" aria-hidden="true" />
+                     ADD CRIMINAL
+                </button>
+            </div>
+            <div class="col-pc-25p col-tb-25p col-mb-50p">
+                <button class="btn btn-white block">
+                    <i class="fa fa-plus-square-o" aria-hidden="true" />
+                    <span class="hidden-mb hidden-tb"> REGISTER NEW WEAPON</span>
+                    <span class="hidden-pc"> ADD WEAPON</span>
+
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Input from '@/components/utils/inputComponent.vue'
+
+export default Vue.extend({
+  components: { Input }
+})
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NewCrime/victimComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-pc-50p col-tb-75p col-mb-100p">
+                <Input label="Victim (optional)" placeholder="Type the name of the victim..." type="text"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-pc-25p col-tb-25p col-mb-50p">
+                <button class="btn btn-white block">
+                    <i class="fa fa-plus-square-o" aria-hidden="true" />
+                     ADD VICTIM
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Input from '@/components/utils/inputComponent.vue'
+
+export default Vue.extend({
+  components: { Input }
+})
+</script>
+
+<style>
+
+</style>
