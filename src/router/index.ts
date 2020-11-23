f9b0f18b89307c0f3ae26e31510f7ea6d6ed2cf3
wallaceantunes <wallace.dantunes@gmail.com>
2020-11-23 04:28:53 -0200

    new routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import crimeList from '../views/crimeList.vue'
+import modalMobile from '../views/modalMobile.vue'
+import newCrime from '../views/newCrime.vue'
+import newWeapon from '../views/newWeapon.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +12,21 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'crimeList',
     component: crimeList
+  },
+  {
+    path: '/new-crime',
+    name: 'newCrime',
+    component: newCrime
+  },
+  {
+    path: '/modal-mobile',
+    name: 'modalMobile',
+    component: modalMobile
+  },
+  {
+    path: '/new-weapon',
+    name: 'newWeapon',
+    component: newWeapon
   }
 ]
 
