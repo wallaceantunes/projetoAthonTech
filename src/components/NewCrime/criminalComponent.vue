32a87dc4370bebdc9a456b60c7c5dadd5622efae
wallaceantunes <wallace.dantunes@gmail.com>
2020-11-24 22:32:51 -0200

    added criminal

--- src/components/NewCrime/criminalComponent.vue
+++ src/components/NewCrime/criminalComponent.vue
@@ -1,27 +1,34 @@
 <template>
     <div class="container">
-        <!-- <div
+        <div
             v-for="(t, index) in criminal"
             :key="index"
-        > -->
-            <div class="row row-nowrap">
+        >
+            <div class="row">
                 <div class="col-pc-50p col-tb-75p col-mb-100p">
                     <Input
                         label="Criminal"
-                        placeholder="Type the name of the criminal..."
-                        type="text"
+                        type="select"
+                        :options="optionCriminals"
+                        :value="t.criminal"
+                        v-on:input="changeCriminal($event, index)"
+                        optionPlaceHolder="Select"
                     />
                 </div>
             </div>
-            <div class="row row-nowrap">
+            <div class="row">
                 <div class="col-pc-25p col-tb-50p col-mb-100p">
                     <Input
                         label="Weapon (optional)"
                         type="select"
+                        :options="optionWeapons"
+                        :value="t.weapon"
+                        v-on:input="changeWeapon($event, index)"
+                        optionPlaceHolder="Select"
                     />
                 </div>
             </div>
-        <!-- </div> -->
+        </div>
         <div class="row">
             <div class="col-pc-25p col-tb-25p col-mb-50p row-margin">
                 <button class="btn btn-white block" @click="addCriminal()">
@@ -29,7 +36,7 @@
                      ADD CRIMINAL
                 </button>
             </div>
-            <div class="col-pc-25p col-tb-25p col-mb-50p">
+            <div class="col-pc-25p col-tb-25p col-mb-50p hidden-pc hidden-tb hidden-mb">
                 <button class="btn btn-white block" @click="addNewWeapon()">
                     <i class="fa fa-plus-square-o" aria-hidden="true" />
                     <span class="hidden-mb hidden-tb"> REGISTER NEW WEAPON</span>
@@ -44,27 +51,80 @@
 <script lang="ts">
 import Vue from 'vue'
 import Input from '@/components/utils/inputComponent.vue'
-
+import { mapState } from 'vuex'
+import { StoreState } from '@/domain/model/storeState'
+import { getCriminals, getWeapons } from '@/store/actions'
+import { Weapon } from '@/domain/model/weapon'
+import { Criminal } from '@/domain/model/criminal'
+interface Data {
+    optionWeapons: Array<{id: number; text: string}>;
+    optionCriminals: Array<{id: number; text: string}>;
+    criminal: Array<{criminal?: string; weapon?: number}>;
+}
 export default Vue.extend({
-  data () {
+  components: { Input },
+  data (): Data {
     return {
+      optionWeapons: [],
+      optionCriminals: [],
       criminal: [{
-        criminal: null,
-        weapon: null
+        criminal: undefined,
+        weapon: undefined
       }]
     }
   },
-  components: { Input },
+  computed: {
+    ...mapState({
+      weapons: (state) => (state as StoreState).weaponModule.weapons,
+      criminals: (state) => (state as StoreState).criminalModule.criminals
+    })
+  },
+  watch: {
+    weapons (newValue: Array<Weapon>) {
+      if (newValue) {
+        this.optionWeapons = newValue.map((ele) => ({
+          id: ele.idWeapon,
+          text: ele.txModel
+        }))
+      }
+    },
+    criminals (newValue: Array<Criminal>) {
+      if (newValue) {
+        this.optionCriminals = newValue.map((ele) => ({
+          id: ele.idCriminal,
+          text: ele.txName
+        }))
+      }
+    }
+  },
+  mounted () {
+    this.$store.dispatch(getWeapons)
+    this.$store.dispatch(getCriminals)
+  },
   methods: {
+    dispatchModel () {
+      this.$emit('changeCriminal', this.criminal)
+    },
+
     addNewWeapon () {
       this.$router.push({ path: '/new-weapon' })
     },
     addCriminal () {
       const data = {
-        criminal: null,
-        weapon: null
+        criminal: undefined,
+        weapon: undefined
       }
       this.criminal.push(data)
+    },
+    changeCriminal (value: any, index: number) {
+      this.criminal[index].criminal = value
+      this.dispatchModel()
+    },
+    changeWeapon (value: any, index: number) {
+      if (value) {
+        this.criminal[index].weapon = value
+        this.dispatchModel()
+      }
     }
   }
 })
